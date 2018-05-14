395df40dc918b4e8ee977a21b8189f302b37ef12
Oksana Krasnytska <o.krasnytska@gmail.com>
2018-05-14 19:05:33 +0300

    Homework 4

--- SetupInstructions.md
+++ SetupInstructions.md
@@ -5,8 +5,6 @@
 2. File explorer → This PC → right click → Properties: look if you have 32 or 64 bit OS
 3. Google → “jdk” → Oracle site → “download” --> if 32bit Windows → jdk 7 → if 64bit → jdk 10 *(last one)* → Accept license agreement → choose OS → "download"
 4. Google --> "Mozilla Firefox download" --> first site --> "download" or upload to a new version
-5. Google --> "Geckodriver download" --> unzip file 
-6. Google --> "maven selenium java" --> link to mvnrepository.com --> copy "dependency" text
 
 ####**Installation:**
 Run installers 1, 3, 4 from downloading → click “Next” ***(don't change anything!)***
@@ -19,9 +17,25 @@ Run installers 1, 3, 4 from downloading → click “Next” ***(don't change an
 - ArtifactID is *“qaauto-23.04.2018”*
 - Project name is *“qaauto-23.04.2018”*
 - Folder - better not to change and remember it
-- open pom.xml file of project --> under "version" insert tags "<dependencies> </dependencies>" --> between this tags insert copied selenium dependency
-- geckodriver file copy --> folder c:/Windows/system32 --> paste file
 
+####Linking tools
+1. Google --> "maven selenium java" --> link to mvnrepository.com --> copy "dependency" text
+open pom.xml file of project --> under "version" insert tags "<dependencies> </dependencies>" --> between this tags insert copied selenium dependency
+2. Google --> "Geckodriver download" --> unzip file 
+geckodriver file copy --> folder c:/Windows/system32 --> paste file
+3. Google --> mvnrepository testNG --> copy dependency for maven --> paste under selenium dependency
 
+####Git Commit & upload:
+
+1. Create new repository at the GIThub (Enter repository name)
+2. Copy link to repository  
+3. Enable version control integration in IDE (VCS  => enable version control integration)
+4. Add link to IDE : VCS => GIT => Remotes
+5. At the IDE select VCS => "Commit..."
+6. At the "Commit Changes" window choose needed files and folders
+7. Enter commit text that describes changes
+8. Select VCS => Git => "Push"
+9. Click on "add origin" link and enter repo url from step 2
+10. Press "Push" button
 
 ###That's all! You are ready to code!
--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -1,14 +1,17 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+
 public class LinkedinLoginTest {
 
     @Test
-    public void successfulLoinTest() {
+    public void successfulLoinTest() throws InterruptedException {
         WebDriver webDriver = new FirefoxDriver();
         webDriver.get("https://www.linkedin.com");
 
@@ -18,12 +21,69 @@ public class LinkedinLoginTest {
         WebElement passwordField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-password']"));
         WebElement signInButton = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login submit-button']"));
         Assert.assertTrue(signInButton.isDisplayed(), "Sign in button is not displayed");
-        emailField.sendKeys("o.a.krasnitskaya@mail.ru");
-        passwordField.sendKeys("10091971");
+        String actualEmail = "o.a.krasnitskaya@mail.ru";
+        String actualPassword = "10091971q";
+        String homePageTitle = "LinkedIn";
+
+        // negative case 1: email is empty, password is right
+        emailField.clear();
+        passwordField.sendKeys(actualPassword);
+        signInButton.click();
+        Assert.assertFalse(webDriver.getCurrentUrl() == homePageTitle, "User is logged in without email inserted");
+        sleep(3000);
+
+        // negative case 2: email is right, password is empty
+        emailField.sendKeys(actualEmail);
+        passwordField.clear();
+        signInButton.click();
+        Assert.assertFalse(webDriver.getCurrentUrl() == homePageTitle, "User is logged in without password inserted");
+        webDriver.navigate().back();
+        sleep(3000);
+
+        // negative case 3: email is right, password is equal to actual, but with caps
+        emailField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-email']"));
+        passwordField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-password']"));
+        signInButton = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login submit-button']"));
+        emailField.sendKeys(actualEmail);
+        passwordField.sendKeys("10091971Q");
+        signInButton.click();
+        Assert.assertFalse(webDriver.getCurrentUrl() == homePageTitle, "User is logged in with wrong password (caps is on)");
+        webDriver.navigate().back();
+        sleep(3000);
+
+        // negative case 4: email is wrong, password is right
+        emailField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-email']"));
+        passwordField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-password']"));
+        signInButton = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login submit-button']"));
+        emailField.sendKeys("@mail.ru");
+        passwordField.sendKeys(actualPassword);
+        signInButton.click();
+        Assert.assertFalse(webDriver.getCurrentUrl() == homePageTitle, "User is logged in with wrong email");
+        webDriver.navigate().back();
+        sleep(3000);
+
+        // negative case 5: email is right, password is wrong
+        emailField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-email']"));
+        passwordField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-password']"));
+        signInButton = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login submit-button']"));
+        emailField.sendKeys(actualEmail);
+        passwordField.sendKeys("@@@");
+        signInButton.click();
+        Assert.assertFalse(webDriver.getCurrentUrl() == homePageTitle, "User is logged in with wrong password");
+        webDriver.navigate().back();
+        sleep(3000);
+
+
+        // positive case
+        emailField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-email']"));
+        passwordField = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login-password']"));
+        signInButton = webDriver.findElement(By.xpath("//form[@class='login-form']/input[@class='login submit-button']"));
+        emailField.sendKeys(actualEmail);
+        passwordField.sendKeys(actualPassword);
         signInButton.click();
         String actualHomePageTitle = webDriver.getTitle();
-        Assert.assertNotEquals(actualLoginPageTitle, actualHomePageTitle, "Page Title did not change aftr sign in");
-        Assert.assertEquals(webDriver.getTitle(), "LinkedIn", "Home page title is wrong");
+        Assert.assertNotEquals(actualLoginPageTitle, actualHomePageTitle, "Page Title did not change after sign in");
+        Assert.assertEquals(webDriver.getTitle(), homePageTitle, "Home page title is wrong");
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page URL is wrong");
         Assert.assertTrue(webDriver.findElement(By.xpath("//a[@href='/feed/']/*[contains(text(), 'Главная')]")).isDisplayed(),
                 "Home button is not present");
